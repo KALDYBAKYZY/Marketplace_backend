5c65c4ee9ea1cc1c5e1929e06ff35b764b445696
KALDYBAKYZY <zhaiylgangulnaz@gmail.com>
2026-04-04 09:36:52 +0500

    sixth commit

--- internal/database/db.go
+++ internal/database/db.go
@@ -12,12 +12,10 @@ var DB *gorm.DB
 
 func Connect() {
 	dsn := "host=localhost user=postgres password=12345678 dbname=marketplace port=5433 sslmode=disable"
-
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
-
 	DB = db
 	fmt.Println("Database connected successfully")
 }
